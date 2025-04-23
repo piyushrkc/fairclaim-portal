2f1a81cefd8cf86a774d594672ada184e6cfe743
Piyush Chaturvedi <piyush@MacBook-P.local>
2025-04-23 11:21:05 +0530

    Added subdomain-based routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomerPortal from './CustomerPortal';
 import AdminDashboard from './AdminDashboard';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<CustomerPortal />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
-  );
+  const hostname = window.location.hostname;
+  const subdomain = hostname.split('.')[0]; // portal or admin
+
+  if (subdomain === 'admin') {
+    return <AdminDashboard />;
+  } else {
+    return <CustomerPortal />;
+  }
 }
 
 export default App;
